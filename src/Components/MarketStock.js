c487c63bcb56edd571e48e848ab1dd468a34c381
Chris Murphy <_chrism_@live.com>
2018-04-29 18:13:00 +0100

    imported lodash

--- src/Components/MarketStock.js
+++ src/Components/MarketStock.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import _ from 'lodash';
 
 const MarketStockSelector = (props) => {
+
   const options = props.stock.map((stockItem, index){
-    return <option key={index} value={index}>{stock.symbol}</option>
+    return <option key={index} value={index}>{stockItem.symbol}</option>
   });
+
+  const sectors = props.stock.map((stockItem, index){
+    return <option key={index} value={index}>{stockItem.sector}</option>
+  })
+
+
 
   const handleChange = (event) => {
     let index = event.target.value
@@ -17,6 +25,11 @@ const MarketStockSelector = (props) => {
        defaultValue="default">
          <option disabled value="default"> search market</option>
          {options}
+       </select>
+       <select
+          id="sector-filter"
+          <option disabled value="default"> by sector</option>
+
        </select>
   )
 }
