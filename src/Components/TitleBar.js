e5026ae0f6bbe79980ca81660e3f60ee8e900019
Angus Malloch <angusbass@hotmail.com>
2018-05-03 09:40:08 +0100

    Added a new header.

--- src/Components/TitleBar.js
+++ src/Components/TitleBar.js
@@ -4,7 +4,7 @@ const TitleBar = (props) => {
   return (
     <div className="title">
       <p className="date-now">{(new Date(new Date().getTime())).toDateString()}</p>
-      <h2>The Stock Market today</h2>
+      <h2>The Beast of Wall Street</h2>
     </div>
   )
 }
